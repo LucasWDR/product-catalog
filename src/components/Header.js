9ec5bcd6deb4e90e099e9427039c95df62712251
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-02-27 16:12:50 -0300

    Create header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Heading, Flex } from "@chakra-ui/react";
+
+const Header = () => {
+    return (
+        <Flex
+            as="nav"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            padding={6}
+            bg="teal.500"
+            color="white"
+        >
+            <Flex align="center" mr={5}>
+                <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+                    Catalago de Produtos
+                </Heading>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google';
+import { Box, Flex } from "@chakra-ui/react";
+
+import Header from '@/components/Header';
+
+export default function Home() {
+  return (
+  <Box>
+    <Header/>
+    <Flex align="center" justifyContent="center">
+    </Flex>
+  </Box>
+  )
+}
