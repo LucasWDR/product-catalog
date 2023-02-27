18e601bdcc8683c7f7c8c9d4915d107a8da0a624
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-02-27 18:08:09 -0300

    Crud for product

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -5,6 +5,7 @@ dbConnection();
 
 export default async function handler(req, res) {
     const { method } = req;
+    const { productId } = req.query;
 
     switch (method) {
         case "GET":
@@ -29,7 +30,28 @@ export default async function handler(req, res) {
             } catch (error) {
                 res.status(500).json({ success: false, error });
             }
+            break;
 
+        case "PUT":
+            try {
+                const { name, value, description, stockQuantity } = req.body;
+                if (!name && !value && !description && !stockQuantity) return "inavalid data";
+                await Product.updateOne({ _id: productId }, { name, value, description, stockQuantity });
+                res.status(200).json({ success: true });
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ success: false, error });
+            }
+            break;
+
+        case "DELETE":
+            try {
+                await Product.deleteOne({ _id: productId });
+                res.status(200).json({ success: true });
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ success: false, error });
+            }
             break;
 
     }
