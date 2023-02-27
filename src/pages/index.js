04ce87472f8563ccfa56c1b6ddaaf0e4a3a54ec0
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-02-27 18:47:33 -0300

    Creating useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,68 @@
-import { Inter } from 'next/font/google';
-import { Box, Button, Flex, FormControl, FormLabel, Input, Table, Tbody, Td, Th, Thead, Tr, VStack } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { Box, Button, Flex, FormControl, FormLabel, Input, Table, Tbody, Td, Th, Thead, Tr, VStack, useToast } from "@chakra-ui/react";
 
 import Header from '@/components/Header';
 
+import api from "../services/api";
+
 export default function Home() {
+
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
+  const [description, setDescription] = useState("");
+  const [stockQuantity, setStockQuantity] = useState("");
+  const [id, setId] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+
+  const isValidFormData = () => {
+    if(!name) {
+      return toast({
+        title: "Preencha o campo nome!",
+        status: "error",
+        duration: 8000,
+        isClosable: true,
+      });
+    }
+    if(!value) {
+      return toast({
+        title: "Preencha o campo Valor!",
+        status: "error",
+        duration: 8000,
+        isClosable: true,
+      });
+    }
+    if(!description) {
+      return toast({
+        title: "Preencha o campo de descrição!",
+        status: "error",
+        duration: 8000,
+        isClosable: true,
+      });
+    }
+    if(!stockQuantity) {
+      return toast({
+        title: "Preencha o campo estoque!",
+        status: "error",
+        duration: 8000,
+        isClosable: true,
+      });
+    }
+
+    if(products.some((product) => product.name === name && product._id  !== id)){
+      return toast({
+        title: "Produto ja cadastrado!!",
+        status: "error",
+        duration: 8000,
+        isClosable: true,
+      });
+    }
+  }
+
+
   return (
     <Box>
       <Header />
@@ -23,24 +82,43 @@ export default function Home() {
           <VStack as="form">
             <FormControl>
               <FormLabel>Nome do Produto</FormLabel>
-              <Input type="text" placeholder="Digite o nome do produto" />
+              <Input 
+              type="text" 
+              placeholder="Digite o nome do produto" 
+              value = {name}
+              onChange = {(e) => setName(e.target.value)}
+              />
             </FormControl>
 
             <FormControl>
               <FormLabel>Valor</FormLabel>
-              <Input type="number" placeholder="Digite o valor do produto" />
+              <Input 
+              type="number" 
+              placeholder="Digite o valor do produto" 
+              value = {value}
+              onChange = {(e) => setValue(e.target.value)}
+              />
             </FormControl>
 
             <FormControl>
               <FormLabel>Descrição</FormLabel>
-              <Input type="text" placeholder="Digite a descrição do produto" wrap="hard" />
+              <Input type="text" 
+              placeholder="Digite a descrição do produto" 
+              wrap="hard" 
+              value = {description}
+              onChange = {(e) => setDescription(e.target.value)}
+              />
             </FormControl>
 
             <FormControl>
               <FormLabel>Estoque</FormLabel>
-              <Input type="number" placeholder="Digite o estoque do produto" />
+              <Input 
+              type="number" 
+              placeholder="Digite o estoque do produto" 
+              value = {stockQuantity}
+              onChange = {(e) => setStockQuantity(e.target.value)}
+              />
             </FormControl>
-
             <Button colorScheme="green" type="submit" mt={6}>
               Adicionar
             </Button>
